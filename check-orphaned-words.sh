b097ea849ef881b7a562c4384a154035eaf08709
James <5511220+Zamiell@users.noreply.github.com>
2022-05-25 06:18:14 -0400

    chore: updating templates

--- check-orphaned-words.sh
+++ check-orphaned-words.sh
@@ -10,7 +10,7 @@ REPO_ROOT="$DIR"
 cd "$REPO_ROOT"
 
 # Do nothing if the configuration file does not exist.
-CSPELL_CONFIGURATION_PATH="$REPO_ROOT/.cspell.json"
+CSPELL_CONFIGURATION_PATH="$REPO_ROOT/cspell.json"
 if ! test -f "$CSPELL_CONFIGURATION_PATH"; then
   exit 0
 fi
@@ -22,14 +22,14 @@ fi
 
 # Make a list of every misspelled word without any custom dictionaries.
 # We need to move the configuration path temporarily or else the cspell command won't work properly.
-CSPELL_CONFIGURATION_PATH_TEMP="$REPO_ROOT/.cspell-temp.json"
+CSPELL_CONFIGURATION_PATH_TEMP="$REPO_ROOT/cspell-temp.json"
 mv "$CSPELL_CONFIGURATION_PATH" "$CSPELL_CONFIGURATION_PATH_TEMP"
 MISSPELLED_WORDS_PATH="/tmp/misspelled-words.txt"
-CSPELL_CONFIGURATION_TEST_PATH="$REPO_ROOT/.cspell-check-unused.json"
+CSPELL_CONFIGURATION_TEST_PATH="$REPO_ROOT/cspell-check-unused.json"
 npx cspell lint --config "$CSPELL_CONFIGURATION_TEST_PATH" --dot --no-progress --no-summary --unique --words-only | sort --ignore-case --unique > "$MISSPELLED_WORDS_PATH"
 mv "$CSPELL_CONFIGURATION_PATH_TEMP" "$CSPELL_CONFIGURATION_PATH"
 
-# Check that each ".cspell.json" word is actually being used.
+# Check that each "cspell.json" word is actually being used.
 echo "Checking for orphaned CSpell configuration words in: $CSPELL_CONFIGURATION_PATH"
 CSPELL_CONFIGURATION_WORDS=$(cat "$CSPELL_CONFIGURATION_PATH" | python -c "import sys, json; print('\n'.join(json.load(sys.stdin)['words']))")
 ONE_OR_MORE_FAILURES=0
--- src/commands/init/createProject.ts
+++ src/commands/init/createProject.ts
@@ -63,6 +63,24 @@ function copyStaticFiles(projectPath: string, verbose: boolean) {
       file.copy(templateFilePath, destinationFilePath, verbose);
     }
   });
+
+  // Rename ".eslintrc.js.template" to ".eslintrc.js". (If it is kept as ".eslintrc.js", then local
+  // linting will fail.)
+  const ESLintConfigPath = path.join(projectPath, ".eslintrc.js.template");
+  const correctESLintConfigPath = path.join(projectPath, ".eslintrc.js");
+  file.rename(ESLintConfigPath, correctESLintConfigPath, verbose);
+
+  // Rename "gitattributes" to ".gitattributes". (If it is kept as ".gitattributes", then it won't
+  // be committed to git.)
+  const gitAttributesPath = path.join(projectPath, "gitattributes");
+  const correctGitAttributesPath = path.join(projectPath, ".gitattributes");
+  file.rename(gitAttributesPath, correctGitAttributesPath, verbose);
+
+  // Rename "cspell.json.template" to "cspell.json". (If it is kept as "cspell.json", then local
+  // spell checking will fail.)
+  const cSpellConfigPath = path.join(projectPath, "cspell.json.template");
+  const correctCSpellConfigPath = path.join(projectPath, "cspell.json");
+  file.rename(cSpellConfigPath, correctCSpellConfigPath, verbose);
 }
 
 /** Copy files that need to have text replaced inside of them. */
